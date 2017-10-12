87287d45bd89d7c5114848c1d7c83e2a16736b42
michaelspeed <michael.the.don@gmail.com>
2017-10-12 18:42:55 +0530

    added antd

--- lessons/src/App.js
+++ lessons/src/App.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PropTestClass from './component/PropTest';
+import {Layout} from 'antd';
 import FirstPage from './component/firstPage'
 import SecondPage from './component/secondPage'
+
+const {Header, Footer, Sider, Content} = Layout;
+
 class App extends Component {
 
   state = {
@@ -49,33 +53,20 @@ class App extends Component {
 
 
   render() {
-      console.log(this)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <Layout>
+          <Header>
+              <div style={{display:'flex', flexDirection:'row'}}>
+                  <span style={{color:'white'}}>Lesson Chat</span>
+              </div>
+          </Header>
+          <Content>
 
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          {this.state.booleanblah ? <p>Super duper blah</p> : <div></div>}
-          <input ref={(blah) => { this.blahinput = blah}}/>
-          <input ref={(goldy) => {this.goldyinput = goldy}}/>
-        <a onClick={this.onClickThis}>clik blah</a>
-          <div>
-              <a onClick={this.clickFirst} >First</a>
+          </Content>
+          <Footer>
 
-          </div>
-          <div>
-              <a onClick={this.clickSecond}>Second</a>
-          </div>
-          {this.state.pageState}
-          <div>
-              <PropTestClass value={'blah'}/>
-          </div>
-      </div>
+          </Footer>
+      </Layout>
     );
   }
 }
