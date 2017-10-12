2de9ab4fb8889e075704d25f5b303fdb8361cbee
jyotishman Parasar <jyotishman.parasar8496@gmail.com>
2017-10-12 18:49:20 +0530

    added firebase

---
+++ lessons/src/firebase/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDI43g1tBe7_9oa3Yns-JKcQBeZsUfzuKI",
+    authDomain: "lesson-chat.firebaseapp.com",
+    databaseURL: "https://lesson-chat.firebaseio.com",
+    projectId: "lesson-chat",
+    storageBucket: "",
+    messagingSenderId: "433814877969"
+  };
+  firebase.initializeApp(config);
